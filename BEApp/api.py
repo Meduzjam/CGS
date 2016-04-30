0472815ae693b0a1caba5d42009f7f98a36d1bb1
Meduzjam <makarovas82@gmail.com>
2016-04-30 11:00:42 +0400

    Убрал авторизацию с REST

--- BEApp/api.py
+++ BEApp/api.py
@@ -1,5 +1,5 @@
 from tastypie.resources import ModelResource
-from tastypie.authentication import ApiKeyAuthentication
+# from tastypie.authentication import ApiKeyAuthentication
 from BEApp.models import Car,Driver
 
 
@@ -7,10 +7,10 @@ class CarResource(ModelResource):
     class Meta:
         queryset = Car.objects.all()
         resource_name = 'Car'
-        authentication = ApiKeyAuthentication()
+        # authentication = ApiKeyAuthentication()
 
 class DriverResource(ModelResource):
     class Meta:
         queryset = Driver.objects.all()
         resource_name = 'Driver'
-        authentication = ApiKeyAuthentication()
+        # authentication = ApiKeyAuthentication()
