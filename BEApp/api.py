4208348970875b6cc5d2a0923f9c04f830560d3e
Meduzjam <makarovas82@gmail.com>
2016-04-30 11:05:00 +0400

    Убрал авторизацию с REST

--- BEApp/api.py
+++ BEApp/api.py
@@ -7,7 +7,7 @@ class CarResource(ModelResource):
     class Meta:
         queryset = Car.objects.all()
         resource_name = 'Car'
-        # authentication = ApiKeyAuthentication()
+        # authentication = ApiKeyAuthentication() //check
 
 class DriverResource(ModelResource):
     class Meta:
