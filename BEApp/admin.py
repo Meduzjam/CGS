2971ecfd60f58733b8061de7dd333ff9436127bd
Meduzjam <makarovas82@gmail.com>
2016-04-30 10:41:30 +0400

    2
    2

--- BEApp/admin.py
+++ BEApp/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Car, Driver
+from django.contrib.auth.admin import UserAdmin	as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Car, Driver,UserProfile
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class UserProfileInline(admin.TabularInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Дополнительная информация'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Car)
 admin.site.register(Driver)
-
+admin.site.register(UserProfile)
 
 # Register your models here.
--- BEApp/models.py
+++ BEApp/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):  
+
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER = (
+        (MALE, 'Чувак'),
+        (FEMALE, 'Чувиха'),
+    )
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    location = models.CharField('Место жительства',max_length=140)  
+    gender = models.CharField('Пол',max_length=1,choices=GENDER,default=MALE)
+    def __str__(self):
+        return u'Profile of user: %s' % self.user.username
 
 class Car(models.Model):
     Car_name = models.CharField('Марка', max_length=200)
